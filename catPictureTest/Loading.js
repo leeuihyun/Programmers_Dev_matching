ba0b9cc9850ae0021127f9d61a4e45c89cf29c2e
leeuihyun <meta212121@naver.com>
2023-01-09 20:03:01 +0900

    [feat] loading Modal append

---
+++ catPictureTest/Loading.js
@@ -0,0 +1,18 @@
+export default function Loading({ target, initialState }) {
+  this.element = document.createElement("div");
+  this.element.className = "Modal Loading";
+  this.state = initialState;
+  target.appendChild(this.element);
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+
+  this.render = () => {
+    this.element.innerHTML = `<div class="content"><img src="./assets/nyan-cat.gif"></div>`;
+    this.element.style.display = this.state ? "block" : "none";
+  };
+
+  this.render();
+}
