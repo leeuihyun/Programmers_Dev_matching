5d13d12a02e3fbd53a87ebc6dd33c44084f9ba67
leeuihyun <meta212121@naver.com>
2023-01-09 21:56:35 +0900

    [feat] Breadcrumb 클릭 시 경로 이동

--- catPictureTest/Breadcrumb.js
+++ catPictureTest/Breadcrumb.js
@@ -1,9 +1,15 @@
-export default function Breadcrumb({ target, initialState }) {
+export default function Breadcrumb({
+  target,
+  initialState,
+  onClick,
+  onRootClick,
+}) {
   this.element = document.createElement("nav");
   this.element.className = "Breadcrumb";
   this.state = initialState;
   target.appendChild(this.element);
-
+  this.onClick = onClick;
+  this.onRootClick = onRootClick;
   this.setState = (nextState) => {
     this.state = nextState;
     this.render();
@@ -11,10 +17,30 @@ export default function Breadcrumb({ target, initialState }) {
 
   this.render = () => {
     this.element.innerHTML = `
-          <div>root</div>
-          ${this.state.map(
-            (v, index) => `<div data-index = "${index}">${v.name}</div>`
-          )}
+          <div class = 'Path' data-node-id = '-1'>root</div>
+          ${this.state
+            .map(
+              (v, index) =>
+                `<div data-node-id = "${index}" class = 'Path'>${v.name}</div>`
+            )
+            .join("")}
       `;
+    this.element.querySelectorAll(".Path").forEach((v, index) => {
+      v.addEventListener("click", (e) => {
+        const { nodeId } = e.target.dataset;
+        const number = Number(nodeId) + 1;
+        if (nodeId) {
+          if (number === 0) {
+            console.log("root");
+            this.onRootClick();
+          } else if (number === this.state.length) {
+            console.log("현재 페이지입니다.");
+          } else {
+            console.log(this.state[nodeId]);
+            this.onClick(this.state[nodeId]);
+          }
+        }
+      });
+    });
   };
 }
